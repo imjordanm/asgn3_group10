268c3b1c8c8986d854e2a658126121dddfaf8f89
jjteare <justin.teare@gmail.com>
2016-09-21 15:23:02 +1200

    Add method to create timeslot in ConsultApp.java Add methods getVetIrd() and insertConsult() to ConsultData.java

--- ConsultData.java
+++ ConsultData.java
@@ -4,9 +4,9 @@ Justin Teare
 */
 
 /** 
- * Support Class ConsultData
- * Stores the data fields for consultation entry
- */
+* Support Class ConsultData
+* Stores the data fields for consultation entry
+*/
 
 
 import java.util.*;
@@ -14,33 +14,69 @@ import java.sql.*;
 
 public class ConsultData {
 
-	private java.sql.Date timeslot; //not sure if this is correct
+	private String timeslot; 
 	private String vet_ird;
 	private int animal_id;
 	private String description;
-
+	
 	//default constructor
-	public ConsultData() { }
-
+  	public ConsultData() { }
+	
 	/**sets the value of the data field timeslot to input parameter value*/
-	public void setTimeslot(java.sql.Date timeslot){      
-		this.timeslot = timeslot;      
-	}//end method
+  	public void setTimeslot(String timeslot){      
+    	this.timeslot = timeslot;      
+  	}//end method
 
 	/**sets the value of vet_ird data field*/
 	public void setVetIrd(String vet_ird){
 		this.vet_ird = vet_ird;
-		System.out.print("VET IRD: ");
-		System.out.println(this.vet_ird);
 	}//end method
 
 	/**set the value of animal_id datafield*/
 	public void setAnimalID (int Animal_id){
 		this.animal_id = animal_id;
 	}//end method
-
+	
 	/**set the value of description data field*/
 	public void setDescription (String description){
 		this.description = description;
 	}
+	
+	/**return stored value of vet_ird*/
+	public String getVetIrd(){
+		return vet_ird;
+	}
+	
+	/**create consultation entry in database*/
+	public void insertConsult(Connection c) {
+	
+		String command = "INSERT INTO consultation "
+						+ "VALUES(TO_DATE(? ,'yyyy-mm-dd hh24:mi:ss'), ?, ?, ?)";
+						
+		PreparedStatement pstmt = null;
+		
+		try {
+			pstmt = c.prepareStatement(command);
+			pstmt.setString(1, timeslot);
+			pstmt.setString(2, vet_ird);
+			pstmt.setInt(3, animal_id);
+			pstmt.setString(4, description);
+			
+			pstmt.executeUpdate();
+		}
+		catch (SQLException e ) {
+		quit(e.getMessage());
+		} finally {
+			try {
+				if (pstmt != null) { pstmt.close(); }
+			} catch (Exception e) {
+				System.out.println("Error in closing " + e.getMessage());
+			}
+		}
+	}
+	// Used to output an error message and exit
+	private void quit(String message) {
+		System.err.println(message);
+		System.exit(1);
+	}	
 }
