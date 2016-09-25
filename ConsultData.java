3770cf6345fa0086b04b205f1ad4427333bc5702
jjteare <justin.teare@gmail.com>
2016-09-25 22:51:18 +1300

    Jordan's changes

--- ConsultData.java
+++ ConsultData.java
@@ -1,96 +1,128 @@
 /*
-*File: ConsultData.java
-*Justin Teare
-*/
+ *File: ConsultData.java
+ *Justin Teare
+ */
 
 /** 
-* Support Class ConsultData
-* Stores the data fields for consultation entry
-*/
+ * Support Class ConsultData
+ * Stores the data fields for consultation entry
+ */
 
 import java.util.*;
 import java.sql.*;
 
 public class ConsultData {
 
-	private String timeslot; 
-	private String vet_ird;
-	private String animal_id;
-	private String owner_id;
-	private String description;
-	private double cost;
+    private String timeslot; 
+    private String vet_ird;
+    private String animal_id;
+    private String owner_id;
+    private String description;
+    private double cost;
 	
-	//default constructor
-  	public ConsultData() { }
+    //default constructor
+    public ConsultData() { }
 	
-	/**sets the value of the data field timeslot to input parameter value*/
-  	public void setTimeslot(String timeslot){
-            this.timeslot = timeslot;
-  	}//end method
+    /**sets the value of the data field timeslot to input parameter value*/
+    public void setTimeslot(String timeslot){
+        this.timeslot = timeslot;
+    }//end method
 
-	/**sets the value of vet_ird data field*/
-	public void setVetIrd(String vet_ird){
-		this.vet_ird = vet_ird;
-	}//end method
+    /**sets the value of vet_ird data field*/
+    public void setVetIrd(String vet_ird){
+        this.vet_ird = vet_ird;
+    }//end method
 
-	/**set the value of animal_id datafield*/
-	public void setAnimalID (String animal_id){
-		this.animal_id = animal_id;
-	}//end method
+    /**set the value of animal_id datafield*/
+    public void setAnimalID (String animal_id){
+        this.animal_id = animal_id;
+    }//end method
 
-	/**set the value of owner_id datafield*/
-        	public void setOwnerID (String owner_id){
-                this.owner_id = owner_id;
-        }//end method
-	
-	/**set the value of description data field*/
-	public void setDescription (String description){
-		this.description = description;
-	}
-	
-	/**set the value of billing data field*/
-        public void setCost (double cost){
-                this.cost = cost;
+    /**set the value of owner_id datafield*/
+    public void setOwnerID (Connection c){
+        // Run query and process results
+        String animal_ID = getAnimalID();
+        
+        String command = "SELECT owner_ID FROM animal " +
+            "WHERE animal_id = ?";
+        
+        PreparedStatement pstmt = null;
+        ResultSet result = null;
+        String owner_id = new String();
+
+        try {
+            pstmt = c.prepareStatement(command);
+            pstmt.setString(1, animal_ID);
+            result = pstmt.executeQuery();
+            result.next();
+            owner_id = result.getString(1);
         }
-	
-	/**return stored value of vet_ird*/
-	public String getVetIrd(){
-		return vet_ird;
-	}
-	
-	/**create consultation entry in database*/
-	public void insertConsult(Connection c) {
-	
-		String command = "INSERT INTO consultation "
-						+ "VALUES(TO_DATE(? ,'yyyy-mm-dd hh24:mi:ss'), ?, ?, ?, ?, ?)";
-						
-		PreparedStatement pstmt = null;
-		
-		try {
-			pstmt = c.prepareStatement(command);
-			pstmt.setString(1, timeslot);
-			pstmt.setString(2, vet_ird);
-			pstmt.setString(3, animal_id);
-			pstmt.setString(4, owner_id);
-			pstmt.setString(5, description);
-			pstmt.setDouble(6, cost);
+        catch (SQLException e ) {
+            quit(e.getMessage());
 
-			pstmt.executeUpdate();
-                        System.out.println("Consultation information:\nDate: " + timeslot + "\nVet: " + vet_ird + "\nAnimal: " + animal_id + "\nOwner: " + owner_id + "\nDescription: " + description + "\nCost: " + cost);
-		}
-		catch (SQLException e ) {
-		quit(e.getMessage());
-		} finally {
-			try {
-				if (pstmt != null) { pstmt.close(); }
-			} catch (Exception e) {
-				System.out.println("Error in closing " + e.getMessage());
-			}
-		}
-	}
-	// Used to output an error message and exit
-	private void quit(String message) {
-		System.err.println(message);
-		System.exit(1);
-	}	
+        } finally {
+            try {
+                if (pstmt != null) { pstmt.close(); }
+                result.close();
+            } catch (Exception e) {
+                System.out.println("Error in closing " + e.getMessage());
+            }
+        }
+        this.owner_id = owner_id;
+    }
+	
+    /**set the value of description data field*/
+    public void setDescription (String description){
+        this.description = description;
+    }
+	
+    /**set the value of billing data field*/
+    public void setCost (double cost){
+        this.cost = cost;
+    }
+	
+    /**return stored value of vet_ird*/
+    public String getVetIrd(){
+        return vet_ird;
+    }
+
+    public String getAnimalID(){
+        return animal_id;
+    } 
+	
+    /**create consultation entry in database*/
+    public void insertConsult(Connection c) {
+	
+        String command = "INSERT INTO consultation "
+            + "VALUES(TO_DATE(? ,'yyyy-mm-dd hh24:mi:ss'), ?, ?, ?, ?, ?)";
+						
+        PreparedStatement pstmt = null;
+		
+        try {
+            pstmt = c.prepareStatement(command);
+            pstmt.setString(1, timeslot);
+            pstmt.setString(2, vet_ird);
+            pstmt.setString(3, animal_id);
+            pstmt.setString(4, owner_id);
+            pstmt.setString(5, description);
+            pstmt.setDouble(6, cost);
+
+            pstmt.executeUpdate();
+            System.out.println("Consultation information:\nDate: " + timeslot + "\nVet: " + vet_ird + "\nOwner: " + owner_id + "\nAnimal: " + animal_id + "\nDescription: " + description + "\nCost: $" + cost);
+        }
+        catch (SQLException e ) {
+            quit(e.getMessage());
+        } finally {
+            try {
+                if (pstmt != null) { pstmt.close(); }
+            } catch (Exception e) {
+                System.out.println("Error in closing " + e.getMessage());
+            }
+        }
+    }
+    // Used to output an error message and exit
+    private void quit(String message) {
+        System.err.println(message);
+        System.exit(1);
+    }	
 }
--- PrescribedMed.java
+++ PrescribedMed.java
@@ -1,13 +1,13 @@
 /*
-*File: ConsultData.java
-*Mutator methods Luyi Qi
-*checkstock(), insertMedicine(), setNewStock() Justin Teare and Jordan McCrae
-*/
+ *File: ConsultData.java
+ *Mutator methods Luyi Qi
+ *checkstock(), insertMedicine(), setNewStock() Justin Teare and Jordan McRae
+ */
 
 /** 
-* Support Class ConsultData
-* Stores the data fields for consultation entry
-*/
+ * Support Class ConsultData
+ * Stores the data fields for consultation entry
+ */
 
 import java.util.*;
 import java.sql.*;
@@ -18,146 +18,135 @@ public class PrescribedMed {
     private int p_num;
     private String name;
     private String dosage;
-	private	int newstock;
+    private int newstock;
 			
     //default constructor
     public PrescribedMed() { }
     
     /**sets the value of the data field quantity to input parameter value*/
-  	public void setQuantity(int quantity){
-            this.quantity = quantity;
-  	}//end method
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }//end method
 
-	/**sets the value of p_num data field*/
-	public void setP_num(int p_num){
-		this.p_num = p_num;
-	}//end method
+    /**sets the value of p_num data field*/
+    public void setP_num(int p_num){
+        this.p_num = p_num;
+    }//end method
 
-	/**set the value of name datafield*/
-	public void setName (String name){
-		this.name = name;
-	}//end method
+    /**set the value of name datafield*/
+    public void setName (String name){
+        this.name = name;
+    }//end method
 	
-	/**set the value of dosage data field*/
-	public void setDosage (String dosage){
-		this.dosage = dosage;
-	}
+    /**set the value of dosage data field*/
+    public void setDosage (String dosage){
+        this.dosage = dosage;
+    }
 	
-	/**retreive quantity prescribed*/	
-	public int getQuantity() {
-		return quantity;
-	}
+    /**retreive quantity prescribed*/	
+    public int getQuantity() {
+        return quantity;
+    }
     
 
-	/**check remaining stock
-	*Query stock level of medicine
-	*Takes Connection object as input
-	*Returns int
-	*/
-	public int checkStock(Connection c) {
-		String command = "SELECT stock FROM medicine "
-				+ "where name = ? and dosage = ?";
-		PreparedStatement pstmt = null;
-		ResultSet result = null;
-        	int stock = 1;
-        	try {
-            		pstmt = c.prepareStatement(command);
-			pstmt.setString(1, name);
-			pstmt.setString(2, dosage);
-			result = pstmt.executeQuery();
-			result.next();
-			stock = result.getInt(1);		
-		}
-        	catch (SQLException e ) {
-            		quit(e.getMessage());
-        	} finally {
-            		try {
-                		if (pstmt != null) { pstmt.close(); }
-            		} catch (Exception e) {
-                		System.out.println("Error in closing " + e.getMessage());
-            		}
-        	}
-		return stock;
-	}
-
-
+    /**check remaining stock
+     *Query stock level of medicine
+     *Takes Connection object as input
+     *Returns int
+     */
+    public int checkStock(Connection c) {
+        String command = "SELECT stock FROM medicine "
+            + "where name = ? and dosage = ?";
+        PreparedStatement pstmt = null;
+        ResultSet result = null;
+        int stock = 1;
+        try {
+            pstmt = c.prepareStatement(command);
+            pstmt.setString(1, name);
+            pstmt.setString(2, dosage);
+            result = pstmt.executeQuery();
+            result.next();
+            stock = result.getInt(1);		
+        }
+        catch (SQLException e ) {
+            quit(e.getMessage());
+        } finally {
+            try {
+                if (pstmt != null) { pstmt.close(); }
+            } catch (Exception e) {
+                System.out.println("Error in closing " + e.getMessage());
+            }
+        }
+        return stock;
+    }
 	
-	/**Create prescribed_in entry in database
-	*Takes connection object as input
-	*/
-	public void insertMedicine(Connection c) {
+    /**Create prescribed_in entry in database
+     *Takes connection object as input
+     */
+    public void insertMedicine(Connection c) {
 	
-		String command = "INSERT INTO prescribed_in "
-						+ "VALUES(?, ?, ?, ?)";
+        String command = "INSERT INTO prescribed_in "
+            + "VALUES(?, ?, ?, ?)";
 						
-		PreparedStatement pstmt = null;
+        PreparedStatement pstmt = null;
 		
-		try {
-			pstmt = c.prepareStatement(command);
-			pstmt.setInt(1, quantity);
-			pstmt.setInt(2, p_num);
-			pstmt.setString(3, name);
-			pstmt.setString(4, dosage);
+        try {
+            pstmt = c.prepareStatement(command);
+            pstmt.setInt(1, quantity);
+            pstmt.setInt(2, p_num);
+            pstmt.setString(3, name);
+            pstmt.setString(4, dosage);
 			
-			pstmt.executeUpdate();
-                        System.out.println("Medicine information:\nQuantity: " 
-				+ quantity + "\nPrescription number: " 
-				+ p_num + "\nMedicine name: " 
-				+ name + "\nDosage: " + dosage);
-		}
-		catch (SQLException e ) {
-		quit(e.getMessage());
-		} finally {
-			try {
-				if (pstmt != null) { pstmt.close(); }
-			} catch (Exception e) {
-				System.out.println("Error in closing " + e.getMessage());
-			}
-		}
-	}
+            pstmt.executeUpdate();
+            System.out.println("\nMedicine information:\nPrescription number: "
+                               + p_num +  "\nMedicine name: " + name + "\nDosage: " + dosage + "\nQuantity: " + quantity);
+        }
+        catch (SQLException e ) {
+            quit(e.getMessage());
+        } finally {
+            try {
+                if (pstmt != null) { pstmt.close(); }
+            } catch (Exception e) {
+                System.out.println("Error in closing " + e.getMessage());
+            }
+        }
+    }
 
-	public void setNewStock(int newstock) {
-		this.newstock = newstock;
-	}	
+    public void setNewStock(int newstock) {
+        this.newstock = newstock;
+    }	
 
-	/**Update stock level in medicines table
-	*takes new stock level and connection object as input
-	*/
-	
-	public void updateStock(int newstock, Connection c) {
-		String command = "UPDATE medicine SET stock = ? WHERE name = ? AND dosage = ?";
+    /**Update stock level in medicines table
+     *takes new stock level and connection object as input
+     */	
+    public void updateStock(int newstock, Connection c) {
+        String command = "UPDATE medicine SET stock = ? WHERE name = ? AND dosage = ?";
 
-                PreparedStatement pstmt = null;
-		System.out.println("Newstock: " + newstock);
-                try {
-                        pstmt = c.prepareStatement(command);
-                        pstmt.setInt(1, newstock);
-                        pstmt.setString(2, name);
-                        pstmt.setString(3, dosage);
+        PreparedStatement pstmt = null;
+        System.out.println("New Stock: " + newstock);
+        try {
+            pstmt = c.prepareStatement(command);
+            pstmt.setInt(1, newstock);
+            pstmt.setString(2, name);
+            pstmt.setString(3, dosage);
 
-                        pstmt.executeUpdate();
-                        /*System.out.println("Medicine information:\nQuantity: "
-                                + quantity + "\nPrescription number: "
-                                + p_num + "\nMedicine name: "
-                                + name + "\nDosage: " + dosage);*/
-                }
-                catch (SQLException e ) {
-                quit(e.getMessage());
-                } finally {
-                        try {
-                                if (pstmt != null) { pstmt.close(); }
-                        } catch (Exception e) {
-                                System.out.println("Error in closing " + e.getMessage());
-                        }
-                }
+            pstmt.executeUpdate();
+        }
+        catch (SQLException e ) {
+            quit(e.getMessage());
+        } finally {
+            try {
+                if (pstmt != null) { pstmt.close(); }
+            } catch (Exception e) {
+                System.out.println("Error in closing " + e.getMessage());
+            }
+        }		
+    }
 
-		
-	}
-
-	// Used to output an error message and exit
-	private void quit(String message) {
-		System.err.println(message);
-		System.exit(1);
-	}	
+    // Used to output an error message and exit
+    private void quit(String message) {
+        System.err.println(message);
+        System.exit(1);
+    }	
 }
 
